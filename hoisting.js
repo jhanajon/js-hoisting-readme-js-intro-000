629c816048701e423c1ed9bf418ed64f7bb447f1
joe fisher <jhanajon@gmail.com>
2018-07-10 00:46:28 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,41 @@
+callMe();
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
-function crazy() {
+// eat();
+//
+// var eat = function(){
+//   console.log("time to eat cake!");
+// }
+//function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  // thisIsCrazy() {
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+
+thisIsCrazy();
+function crazy() {
+var thisIsCrazy = function thisIsCrazy() {
+    console.log("hey!!!");
   }
 }
+
+
+//original
+// function crazy() {
+//   // fix the code in here:
+//   thisIsCrazy();
+//
+//   var thisIsCrazy = function (){
+//     console.log("hey!!!")
+//   }
+// }
 
 function sayMyName() {
   // fix the code in here:
@@ -28,3 +48,15 @@ function sayMyName() {
     var name = "Kristin";
   }
 }
+
+// function sayMyName() {
+//   // fix the code in here:
+//   var name = "Cricky";
+//
+//   sayMy();
+//
+//   function sayMy() {
+//     console.log(name);
+//     var name = "Kristin";
+//   }
+// }
