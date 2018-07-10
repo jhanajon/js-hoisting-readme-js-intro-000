ccaaf7b0217e714c0d87a28f2f53cf559d4aa4a1
joe fisher <jhanajon@gmail.com>
2018-07-10 01:04:35 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -9,6 +9,18 @@ function callMe() {
   return lyric;
 }
 
+//working version
+// callMe();
+//
+// function callMe() {
+//   console.log("I just met you...");
+//   console.log("and this is crazy..");
+//   console.log("but here's my number..");
+//   console.log("so call me");
+//   var lyric = "maybe";
+//   return lyric;
+// }
+
 
 // eat();
 //
