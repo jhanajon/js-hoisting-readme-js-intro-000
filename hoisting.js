202e6d326090a8eedb44b82a5e6de9b0524c5344
joe fisher <jhanajon@gmail.com>
2018-07-10 01:38:31 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -31,9 +31,10 @@ function callMe() {
 function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+  
   }
   thisIsCrazy();
+    console.log("hey!!!")
 }
 
 
@@ -53,12 +54,13 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
 
 // function sayMyName() {
